5eaef5959cb8c7034b346993bbde5194ef6a0b8d
Murat Yalçın <muratyalcin.my@outlook.com>
2019-11-27 23:45:49 +0300

    Create table parser implemented. (Murat, Feyza, Tugay, Mehmet)

--- sql_constraint_checker.py
+++ sql_constraint_checker.py
@@ -1,7 +1,6 @@
 from typing import NamedTuple
 import re
 
-
 CONSTRAINTS = [
 	"NOT NULL",
 	"UNIQUE",
@@ -20,17 +19,24 @@ class TableConstraints(NamedTuple):
 
 def read_sql_file(file_path) -> str:
 	with open(file_path, 'r') as file:
-		sql = file.read().replace("\n", " ")
-	return sql
+		sql_file = file.read().replace("\n", " ")
+	return sql_file
 
 
 def find_keyword_blocks(keyword_name: str, txt: str) -> list:
-	parsed = re.split(keyword_name, txt)
+	parsed = txt.split(keyword_name)
 	return [" ".join(p.split(";")[0].split()) for p in parsed[1:]]
 
 
-def get_constraints(file) -> TableConstraints:
-	pass
+def create_table_parser(ct_block: str) -> TableConstraints:
+	table_name = ct_block.split()[0]
+	constraint = {}
+	idx = ct_block.index("(")
+	parsed = ct_block[idx+1:-1]
+	for p in parsed.split(","):
+		val = [word for word in CONSTRAINTS if word in p.upper()]
+		constraint[p.split()[0]] = val
+	return TableConstraints(table_name, constraint)
 
 
 def check_diff(old_file: TableConstraints, new_file: TableConstraints) -> bool:
@@ -38,8 +44,10 @@ def check_diff(old_file: TableConstraints, new_file: TableConstraints) -> bool:
 
 
 def main():
-	file = read_sql_file("sample_input.sql")
-	print(find_keyword_blocks("CREATE TABLE", file))
+	file = read_sql_file("difference1-test.sql")
+	ct_block_list = find_keyword_blocks("CREATE TABLE", file)
+	for ct_block in ct_block_list:
+		print(create_table_parser(ct_block))
 
 
 if __name__ == "__main__":
