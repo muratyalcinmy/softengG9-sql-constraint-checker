2166200d42a3c4238bb87dac7a4b69edc45fe045
Murat Yalçın <muratyalcin.my@outlook.com>
2019-12-12 13:26:51 +0300

    JSON, requests and logger added.

--- sql_constraint_checker.py
+++ sql_constraint_checker.py
@@ -1,12 +1,21 @@
+import os
+import json
+import requests
+import logging.config
+from base64 import b64decode
 from typing import NamedTuple
+
+logging_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'log.ini')
+logging.config.fileConfig(fname=logging_path, disable_existing_loggers=False)
+logger = logging.getLogger(__name__)
 
 
 class SQLParse:
-	def __init__(self, filename_sql):
-		self.text = self.read_as_text(filename_sql).upper()
+	def __init__(self, sql_text):
+		self.upper_text = sql_text.upper()
 
 	def find_keyword_blocks(self, keyword_name: str) -> [str]:
-		detect_bgn = self.text.split(keyword_name)[1:]
+		detect_bgn = self.upper_text.split(keyword_name)[1:]
 		parsed_blocks = []
 		for each in detect_bgn:
 			detect_end = each.split(';')[0]
@@ -19,11 +28,6 @@ class SQLParse:
 		with open(file_path, 'r') as f:
 			return f.read().split()
 
-	@staticmethod
-	def read_as_text(file_path) -> str:
-		with open(file_path, 'r') as f:
-			return f.read().replace('\n', ' ')
-
 
 class TableParse(SQLParse):
 	class TableConstraints(NamedTuple):
@@ -31,13 +35,13 @@ class TableParse(SQLParse):
 		constraint: dict
 
 		def __repr__(self):
-			out = '* TABLE NAME:\n{}\n* CONSTRAINT:'.format(self.table_name)
+			out = 'TABLE NAME: {} | CONSTRAINT: '.format(self.table_name)
 			for key, val in self.constraint.items():
-				out += '\n' + key + ' ' + val
+				out += '(' + key + ' - ' + val + ') '
 			return out
 
-	def __init__(self, filename_sql, filename_constraints, filename_data_types):
-		super(TableParse, self).__init__(filename_sql)
+	def __init__(self, sql_text, filename_constraints, filename_data_types):
+		super(TableParse, self).__init__(sql_text)
 		self.__constraints = self.read_as_list(filename_constraints)
 		self.__data_types = self.read_as_list(filename_data_types)
 
@@ -62,24 +66,24 @@ class TableParse(SQLParse):
 
 
 class TableCheck:
-	def __init__(self, filename_sql_old, filename_sql_new, filename_constraints, filename_data_types):
-		self.__table_old = TableParse(filename_sql_old, filename_constraints, filename_data_types).parse()
-		self.__table_new = TableParse(filename_sql_new, filename_constraints, filename_data_types).parse()
+	def __init__(self, old_text, new_text, filename_constraints, filename_data_types):
+		self.__table_old = TableParse(old_text, filename_constraints, filename_data_types).parse()
+		self.__table_new = TableParse(new_text, filename_constraints, filename_data_types).parse()
 		self.__check_new = []
 		self.__check_err = []
 		self.output = self.__check_diff()
 
 	def diff(self):
 		if self.output:
-			print('-----> ALLOWED!')
+			logger.info('-----> ALLOWED!')
 			for each in self.__check_new:
-				print('-----> ADDED:')
-				print(each)
+				logger.info('-----> ADDED:')
+				logger.info(each)
 		else:
-			print('-----> NOT ALLOWED!')
+			logger.info('-----> NOT ALLOWED!')
 			for each in self.__check_err:
-				print('-----> ERROR:')
-				print(each)
+				logger.info('-----> ERROR:')
+				logger.info(each)
 		return self.output
 
 	def __check_diff(self) -> bool:
@@ -89,8 +93,8 @@ class TableCheck:
 				for new_col_name, old_col_name in zip(new.constraint, old.constraint):
 					if new_col_name == old_col_name:
 						if new.constraint[new_col_name] != old.constraint[old_col_name]:
-							tbl = 'TABLE NAME: {}\n'.format(new.table_name)
-							col = 'COLUMN NAME: {}\n'.format(new_col_name)
+							tbl = 'TABLE NAME: {} | '.format(new.table_name)
+							col = 'COLUMN NAME: {} | '.format(new_col_name)
 							dif = 'DIFFERENCE: \"{}\" != \"{}\"'.format(new.constraint[new_col_name],
 																		old.constraint[old_col_name])
 							self.__check_err.append(tbl + col + dif)
@@ -99,9 +103,20 @@ class TableCheck:
 		return False if self.__check_err else True
 
 
-def main():
-	TableCheck('difference1-test.sql', 'difference2-test.sql', 'constraints.txt', 'data_types.txt').diff()
+def main(json_obj):
+	data = json.loads(json_obj)
+	old_text = b64decode(data['old']).decode('utf-8')
+	new_text = b64decode(data['new']).decode('utf-8')
 
+	output = dict()
+	output['name'] = data['name']
+	output['reminder'] = data['reminder']
+	output['result'] = TableCheck(old_text, new_text, 'constraints.txt', 'data_types.txt').diff()
+	output['op'] = 'check'
+	output['origin'] = 9
+	output['destination'] = 8
 
-if __name__ == '__main__':
-	main()
+	logger.info('-----> RESULT: ' + str(output['result']))
+
+	# r = requests.post('http://localhost:8081/', json=output)
+	# logger.info('-----> RESPONSE: ' + str(r.status_code))
